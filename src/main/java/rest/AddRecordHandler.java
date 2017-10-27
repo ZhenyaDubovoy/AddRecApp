90807ce2d0b04187327e66d6ea147e55887ac8aa
Евгений Дубовой <evgeniydubovoy96@gmail.com>
2017-10-27 21:08:38 +0300

    jquery added

--- src/main/java/rest/AddRecordHandler.java
+++ src/main/java/rest/AddRecordHandler.java
@@ -4,9 +4,11 @@ import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import model.Record;
+import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by Евгений Дубовой on 19.10.2017.
@@ -18,9 +20,9 @@ public class AddRecordHandler implements HttpHandler {
         Gson gson = new Gson();
 
         if (httpExchange.getRequestMethod().equals("POST")){
-            Record addedRecord = new Record("","");
-            addedRecord = gson.fromJson(new InputStreamReader(httpExchange.getRequestBody()), Record.class);
-            String body = addedRecord.getBody();
+            //Record addedRecord = new Record("","");
+            Record addedRecord = gson.fromJson(new InputStreamReader(httpExchange.getRequestBody()), Record.class);
+            Record.records.add(addedRecord);
         }
 
         System.out.println(Record.records.isEmpty());
